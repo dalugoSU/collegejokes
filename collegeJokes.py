61e223254b4f020e81ea8072d3bc88fc7844750f
Daniel Lugo <dalugo@syr.edu>
2021-05-11 12:39:09 -0400

    First API commit

---
+++ collegeJokes.py
@@ -0,0 +1,72 @@
+from flask import Flask
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from jokes import jokes
+import random
+
+
+class Joke(Resource):
+    
+    def get(self, id_=0):
+        if id==0:
+            return random.choice(jokes), 200
+        
+        for joke in jokes:
+            if joke["id"] == id_:
+                return joke, 200
+        
+        return "ID out of bounds...", 404
+    
+    def post(self, id_):
+        parser = reqparse.RequestParser()
+        parser.add_argument("joke")
+        parser.add_argument("joke_response")
+        params = parser.parse_args()
+        
+        for joke in jokes:
+            if id_ == joke["id"]:
+                return f"Joke with id {id_} already exists", 400
+        
+        joke = {
+            "id": int(id_),
+            "joke": params["joke"],
+            "joke_response": params["joke_response"]
+        }
+        
+        jokes.append(joke)
+        return joke, 201
+    
+    def put(self, id_):
+        parser = reqparse.RequestParser()
+        parser.add_argument("joke")
+        parser.add_argument("joke_response")
+        params = parser.parse_args()
+        
+        for joke in jokes:
+            if id_ == jokes["id"]:
+                joke["joke"] = params["joke"]
+                joke["joke_response"] = params["joke_response"]
+                return joke, 200
+        
+        joke = {
+            "id": id_,
+            "joke": params["joke"],
+            "joke_response": params["joke_response"]
+        }
+        
+        jokes.append(joke)
+        return joke, 201
+    
+    def delete(self, id_):
+        global jokes
+        jokes = [joke for joke in jokes if joke["id"] != id_]
+        return f"Joke with id {id_} is deleted", 200
+        
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(Joke, "/jokes", "/jokes", "/jokes/<int:id>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ jokes.py
@@ -0,0 +1,35 @@
+jokes = [
+    {
+        "id": 0,
+        "joke": "How do you know that you have been in college too long?",
+        "joke_response": "Your parents are running out of money!"
+    },
+    {
+         "id": 1,
+        "joke": "Why did the sun skip college?",
+        "joke_response": "It already has a million degrees."
+
+    },
+    {
+        "id": 2,
+        "joke": "What did the buffalo say to his son when he left for college?",
+        "joke_response": "Bi-son!"
+
+    },
+    {
+        "id": 3,
+        "joke": "If pilgrims traveled on the Mayflower, what do college students travel on?",
+        "joke_response": "Scholar-ships"
+    },
+    {
+        "id": 4,
+        "joke": "A new student at Harvard stopped an upperclassman and asked, 'Where’s the library at?'",
+        "joke_response": "The upperclassman said, 'Never end a sentence with a preposition. Cops do it on TV, but it isn’t proper, so to speak.' The new student said, 'Pardon me. Where’s the library at, MORON?'"
+    },
+    {
+        "id": 5,
+        "joke": "What do you call hiking U.S. college students?",
+        "joke_response": "The walking debt!"
+    }
+    
+]
